75a337b2a781f31b156947fdd87afd73a8048faa
Steve Kinney <hello@stevekinney.net>
2013-04-22 09:32:00 -0400

    Add in script file

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
   
-  
+  Mousetrap.bind('x', function() {
+    
+    var w = parseInt($('#herbert').attr('width'));
+    
+    $('#herbert').animate({
+      width: w * 1.10 +'px',
+    });
+  });
   
 });
